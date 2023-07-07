e8be7328798e783c24b97be3c51a28928d95ed36
101189766+simonmcnair@users.noreply.github.com <101189766+simonmcnair@users.noreply.github.com>
2023-07-07 17:29:25 +0100

    Updating file - Revision 3

--- compare_dir_md5.py
+++ compare_dir_md5.py
@@ -1,74 +1,68 @@
 import os
-import json
 import hashlib
+import json
 import csv
 
-# define the directories to compare
-dir1 = '/path/to/dir1'
-dir2 = '/path/to/dir2'
+# define the two directories to compare
+dir1 = 'path/to/dir1'
+dir2 = 'path/to/dir2'
 
-# define the cache file to store the file paths and hashes
-cache_file = 'cache.json'
+# define the cache file name
+cache_file = 'hashes.json'
 
-# define the output csv files
-diff_file = 'differences.csv'
+# define the output file names
+diff_file = 'diff.csv'
 identical_file = 'identical.csv'
 
-# check if cache file exists
+# helper function to generate the hash of a file
+def generate_hash(file_path):
+    sha256 = hashlib.sha256()
+    with open(file_path, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b''):
+            sha256.update(chunk)
+    return sha256.hexdigest()
+
+# check if the cache file exists and load its contents if it does
 if os.path.exists(cache_file):
     with open(cache_file, 'r') as f:
         cache = json.load(f)
 else:
     cache = {}
 
-# loop through each directory and generate hashes for each file
+# iterate over the files in both directories and generate hashes
 for directory in [dir1, dir2]:
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            file_path = os.path.join(root, file)
-            if file_path in cache:
-                # skip hash generation if hash already exists for file path
-                continue
-            with open(file_path, 'rb') as f:
-                file_hash = hashlib.sha256(f.read()).hexdigest()
-                cache[file_path] = file_hash
-                # update the cache file with the new hash
-                with open(cache_file, 'w') as f:
-                    json.dump(cache, f)
+    files = os.listdir(directory)
+    files.sort()  # sort the files by name
+    for file_name in files:
+        file_path = os.path.join(directory, file_name)
+        if file_path not in cache:
+            hash_value = generate_hash(file_path)
+            cache[file_path] = hash_value
+            with open(cache_file, 'w') as f:
+                json.dump(cache, f)
 
-# compare the hashes for each directory and create csv files
-diff_hashes = {}
-identical_hashes = {}
-for file_path, file_hash in cache.items():
-    if file_path.startswith(dir1):
-        file_path2 = file_path.replace(dir1, dir2)
-        if os.path.exists(file_path2):
-            with open(file_path2, 'rb') as f:
-                file_hash2 = hashlib.sha256(f.read()).hexdigest()
-            if file_hash != file_hash2:
-                # add to differences if hashes don't match
-                diff_hashes[file_path] = (file_hash, file_path2, file_hash2)
+# compare the hashes of the files in the two directories
+differences = []
+identical_groups = []
+for file_path in cache:
+    if dir1 in file_path:
+        other_path = file_path.replace(dir1, dir2)
+        if other_path in cache:
+            if cache[file_path] != cache[other_path]:
+                differences.append((file_path, other_path))
             else:
-                # add to identical groups if hashes match
-                identical_hashes.setdefault(file_hash, []).append(file_path)
-                identical_hashes[file_hash].append(file_path2)
-        else:
-            # add to differences if file doesn't exist in dir2
-            diff_hashes[file_path] = (file_hash, None, None)
-    elif file_path.startswith(dir2):
-        # already processed in previous if block
-        continue
+                identical_groups.append((cache[file_path], file_path, other_path))
 
-# write differences to csv file
-with open(diff_file, 'w') as f:
+# write the differences to a CSV file
+with open(diff_file, 'w', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(['File Path 1', 'Hash 1', 'File Path 2', 'Hash 2'])
-    for file_path, hashes in diff_hashes.items():
-        writer.writerow([file_path, hashes[0], hashes[1], hashes[2]])
+    writer.writerow(['File in ' + dir1, 'Hash in ' + dir1, 'File in ' + dir2, 'Hash in ' + dir2])
+    for diff in differences:
+        writer.writerow(diff)
 
-# write identical groups to csv file
-with open(identical_file, 'w') as f:
+# write the identical groups to a CSV file
+with open(identical_file, 'w', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(['Hash', 'File Paths'])
-    for file_hash, file_paths in identical_hashes.items():
-        writer.writerow([file_hash, ', '.join(file_paths)])
+    writer.writerow(['Hash', 'File in ' + dir1, 'File in ' + dir2])
+    for group in identical_groups:
+        writer.writerow(group)
