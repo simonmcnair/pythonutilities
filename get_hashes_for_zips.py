239a34afa4998900a35704cd5f5923ac320ce33e
101189766+simonmcnair@users.noreply.github.com <101189766+simonmcnair@users.noreply.github.com>
2023-07-07 17:31:22 +0100

    Updating file - Revision 5

--- get_hashes_for_zips.py
+++ get_hashes_for_zips.py
@@ -1,75 +1,67 @@
 import os
-import zipfile
-import rarfile
-import tarfile
-import hashlib
 import csv
+import hashlib
 import shutil
+import zipfile
+import tarfile
+import rarfile
 import py7zr
 import msilib
-from datetime import datetime
-import tempfile
 
 
 def process_archive(file_path, log_writer):
+    archive_dir, archive_name = os.path.split(file_path)
+    archive_type = archive_name.split('.')[-1].lower()
     try:
-        archive_name = os.path.basename(file_path)
-        archive_dir = os.path.dirname(file_path)
-        archive_type = ''
-        if zipfile.is_zipfile(file_path):
-            archive_type = 'ZIP'
-            archive = zipfile.ZipFile(file_path, 'r')
-        elif rarfile.is_rarfile(file_path):
-            archive_type = 'RAR'
-            archive = rarfile.RarFile(file_path, 'r')
-        elif file_path.endswith('.tar'):
-            archive_type = 'TAR'
-            archive = tarfile.open(file_path, 'r')
-        elif file_path.endswith('.7z'):
-            archive_type = '7Z'
-            archive = py7zr.SevenZipFile(file_path, 'r')
-        elif file_path.endswith('.msi'):
-            archive_type = 'MSI'
-            archive = msilib.OpenDatabase(file_path, msilib.MSIDBOPEN_READONLY)
+        if archive_type == 'zip':
+            with zipfile.ZipFile(file_path, 'r') as zip_file:
+                for zip_info in zip_file.infolist():
+                    with zip_file.open(zip_info) as file:
+                        sha1 = hashlib.sha1(file.read()).hexdigest()
+                        log_writer.writerow([get_timestamp(), archive_dir, archive_name, zip_info.filename, os.path.basename(zip_info.filename), sha1])
+        elif archive_type == 'msi':
+            db = msilib.OpenDatabase(file_path, msilib.MSIDBOPEN_READONLY)
+            view = db.OpenView("SELECT * FROM _Streams")
+            view.Execute(None)
+            while True:
+                rec = view.Fetch()
+                if rec is None:
+                    break
+                stream_name = rec.GetString(1)
+                if stream_name.startswith("#"):
+                    continue
+                with db.OpenStream(stream_name) as file:
+                    sha1 = hashlib.sha1(file.read()).hexdigest()
+                    log_writer.writerow([get_timestamp(), archive_dir, archive_name, stream_name, os.path.basename(stream_name), sha1])
+        elif archive_type == 'tar':
+            with tarfile.open(file_path, 'r') as tar_file:
+                for tar_info in tar_file.getmembers():
+                    if tar_info.isfile():
+                        with tar_file.extractfile(tar_info) as file:
+                            sha1 = hashlib.sha1(file.read()).hexdigest()
+                            log_writer.writerow([get_timestamp(), archive_dir, archive_name, tar_info.name, os.path.basename(tar_info.name), sha1])
+        elif archive_type == 'rar':
+            with rarfile.RarFile(file_path, 'r') as rar_file:
+                for rar_info in rar_file.infolist():
+                    if rar_info.isfile():
+                        with rar_file.open(rar_info) as file:
+                            sha1 = hashlib.sha1(file.read()).hexdigest()
+                            log_writer.writerow([get_timestamp(), archive_dir, archive_name, rar_info.filename, os.path.basename(rar_info.filename), sha1])
+        elif archive_type == '7z':
+            with py7zr.SevenZipFile(file_path, mode='r') as seven_zip:
+                for file_info in seven_zip.getnames():
+                    with seven_zip.open(file_info) as file:
+                        sha1 = hashlib.sha1(file.read()).hexdigest()
+                        log_writer.writerow([get_timestamp(), archive_dir, archive_name, file_info, os.path.basename(file_info), sha1])
         else:
-            return
-        
-        # Extract files to a temporary directory
-        temp_dir = tempfile.mkdtemp()
-        archive.extractall(temp_dir)
-        
-        # Process extracted files
-        for root, dirs, files in os.walk(temp_dir):
-            for file_name in files:
-                file_path = os.path.join(root, file_name)
-                with open(file_path, 'rb') as f:
-                    file_bytes = f.read()
-                file_checksum = hashlib.sha1(file_bytes).hexdigest()
-                log_writer.writerow([datetime.now(), archive_dir, archive_name, file_path, os.path.basename(file_name), file_checksum])
-        
-        archive.close()
-        
-        # Delete temporary directory
-        shutil.rmtree(temp_dir)
-        
+            print(f"Unsupported archive type: {archive_type}")
     except Exception as e:
         print(f"Error processing archive {file_path}: {e}")
-        shutil.move(file_path, os.path.join(archive_dir, 'badarchives'))
+        shutil.move(file_path, os.path.join(os.path.dirname(file_path), 'badarchives', os.path.basename(file_path)))
 
 
-def process_directory(directory_path, log_file_path):
-    with open(log_file_path, 'a', newline='') as log_file:
+def process_directory(directory_path):
+    log_file_path = os.path.join(directory_path, 'checksum_log.csv')
+    with open(log_file_path, 'w', newline='') as log_file:
         log_writer = csv.writer(log_file)
-        for root, dirs, files in os.walk(directory_path):
-            for file_name in files:
-                file_path = os.path.join(root, file_name)
-                process_archive(file_path, log_writer)
-
-
-if __name__ == '__main__':
-    directory_path = input("Enter directory path: ")
-    log_file_path = input("Enter log file path: ")
-    if not os.path.isdir(directory_path):
-        print(f"{directory_path} is not a valid directory.")
-    else:
-        process_directory(directory_path, log_file_path)
+        log_writer
