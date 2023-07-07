3a7ac13195650ae37877fcd2f7c38f2d4abf8993
101189766+simonmcnair@users.noreply.github.com <101189766+simonmcnair@users.noreply.github.com>
2023-07-07 17:29:25 +0100

    Updating file - Revision 2

--- compare_dir_md5.py
+++ compare_dir_md5.py
@@ -1,61 +1,74 @@
 import os
+import json
 import hashlib
 import csv
 
-def get_sha256(file_path):
-    """Return the SHA256 hash of a file."""
-    sha256 = hashlib.sha256()
-    print("Started creating hash for " + file_path)
-    with open(file_path, 'rb') as f:
-        while True:
-            data = f.read(65536)
-            if not data:
-                break
-            sha256.update(data)
-    print("Created hash for " + file_path)
-    return sha256.hexdigest()
+# define the directories to compare
+dir1 = '/path/to/dir1'
+dir2 = '/path/to/dir2'
 
-def compare_directories(dir1, dir2, output_file):
-    """Compare the SHA256 hashes of files in two directories and write the results to a CSV file."""
-    dir1_files = set()
-    dir2_files = set()
-    for root, dirs, files in os.walk(dir1):
-        for name in files:
-            dir_to_add = os.path.join(root, name)
-            dir1_files.add(dir_to_add)
-            print("adding " + dir_to_add)
-    for root, dirs, files in os.walk(dir2):
-        for name in files:
-            dir_to_add = os.path.join(root, name)
-            dir2_files.add(dir_to_add)
-            print("adding " + dir_to_add)
-    dir1_hashes = {}
-    dir2_hashes = {}
-    for file_path in dir1_files:
-        hash = get_sha256(file_path)
-        print (hash + " is hash for " + file_path)
-        dir1_hashes[file_path] = hash
-    for file_path in dir2_files:
-        hash = get_sha256(file_path)
-        print (hash + " is hash for " + file_path)
-        dir2_hashes[file_path] = hash
-    with open(output_file, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(["File", "Directory 1 SHA256", "Directory 2 SHA256", "Status"])
-        for file_path, hash1 in dir1_hashes.items():
-            if file_path in dir2_hashes:
-                hash2 = dir2_hashes[file_path]
-                if hash1 == hash2:
-                    status = "Match"
-                else:
-                    status = "Different"
+# define the cache file to store the file paths and hashes
+cache_file = 'cache.json'
+
+# define the output csv files
+diff_file = 'differences.csv'
+identical_file = 'identical.csv'
+
+# check if cache file exists
+if os.path.exists(cache_file):
+    with open(cache_file, 'r') as f:
+        cache = json.load(f)
+else:
+    cache = {}
+
+# loop through each directory and generate hashes for each file
+for directory in [dir1, dir2]:
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            if file_path in cache:
+                # skip hash generation if hash already exists for file path
+                continue
+            with open(file_path, 'rb') as f:
+                file_hash = hashlib.sha256(f.read()).hexdigest()
+                cache[file_path] = file_hash
+                # update the cache file with the new hash
+                with open(cache_file, 'w') as f:
+                    json.dump(cache, f)
+
+# compare the hashes for each directory and create csv files
+diff_hashes = {}
+identical_hashes = {}
+for file_path, file_hash in cache.items():
+    if file_path.startswith(dir1):
+        file_path2 = file_path.replace(dir1, dir2)
+        if os.path.exists(file_path2):
+            with open(file_path2, 'rb') as f:
+                file_hash2 = hashlib.sha256(f.read()).hexdigest()
+            if file_hash != file_hash2:
+                # add to differences if hashes don't match
+                diff_hashes[file_path] = (file_hash, file_path2, file_hash2)
             else:
-                status = "Missing in Directory 2"
-                hash2 = ""
-            writer.writerow([file_path, hash1, hash2, status])
-        for file_path, hash2 in dir2_hashes.items():
-            if file_path not in dir1_hashes:
-                writer.writerow([file_path, "", hash2, "Missing in Directory 1"])
+                # add to identical groups if hashes match
+                identical_hashes.setdefault(file_hash, []).append(file_path)
+                identical_hashes[file_hash].append(file_path2)
+        else:
+            # add to differences if file doesn't exist in dir2
+            diff_hashes[file_path] = (file_hash, None, None)
+    elif file_path.startswith(dir2):
+        # already processed in previous if block
+        continue
 
-# Example usage:
-compare_directories("/srv/External_6TB_1/root/Videos", "/srv/mergerfs/data/Video2", "output.csv")
+# write differences to csv file
+with open(diff_file, 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(['File Path 1', 'Hash 1', 'File Path 2', 'Hash 2'])
+    for file_path, hashes in diff_hashes.items():
+        writer.writerow([file_path, hashes[0], hashes[1], hashes[2]])
+
+# write identical groups to csv file
+with open(identical_file, 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(['Hash', 'File Paths'])
+    for file_hash, file_paths in identical_hashes.items():
+        writer.writerow([file_hash, ', '.join(file_paths)])
