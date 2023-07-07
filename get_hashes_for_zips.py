52b2c9c0ef253b5e590858d4c216a7d4b6115479
101189766+simonmcnair@users.noreply.github.com <101189766+simonmcnair@users.noreply.github.com>
2023-07-07 17:31:21 +0100

    Updating file - Revision 4

--- get_hashes_for_zips.py
+++ get_hashes_for_zips.py
@@ -5,6 +5,11 @@ import tarfile
 import hashlib
 import csv
 import shutil
+import py7zr
+import msilib
+from datetime import datetime
+import tempfile
+
 
 def process_archive(file_path, log_writer):
     try:
@@ -29,45 +34,42 @@ def process_archive(file_path, log_writer):
         else:
             return
         
-        for file_info in archive.infolist():
-            if not file_info.is_dir():
-                file_name = file_info.filename
-                file_bytes = archive.read(file_name)
+        # Extract files to a temporary directory
+        temp_dir = tempfile.mkdtemp()
+        archive.extractall(temp_dir)
+        
+        # Process extracted files
+        for root, dirs, files in os.walk(temp_dir):
+            for file_name in files:
+                file_path = os.path.join(root, file_name)
+                with open(file_path, 'rb') as f:
+                    file_bytes = f.read()
                 file_checksum = hashlib.sha1(file_bytes).hexdigest()
-                file_path = os.path.join(archive_dir, file_name)
-                log_writer.writerow([archive_dir, archive_name, file_path, os.path.basename(file_name), file_checksum])
+                log_writer.writerow([datetime.now(), archive_dir, archive_name, file_path, os.path.basename(file_name), file_checksum])
         
         archive.close()
         
-        # delete extracted files
-        if archive_type in ['ZIP', 'RAR', '7Z']:
-            shutil.rmtree(os.path.join(archive_dir, archive_name + '_extracted'))
-        elif archive_type == 'TAR':
-            for member in archive.getmembers():
-                if member.isreg():
-                    os.remove(os.path.join(archive_dir, member.name))
-        elif archive_type == 'MSI':
-            pass  # do nothing since MSI files don't extract to a directory
+        # Delete temporary directory
+        shutil.rmtree(temp_dir)
+        
     except Exception as e:
         print(f"Error processing archive {file_path}: {e}")
         shutil.move(file_path, os.path.join(archive_dir, 'badarchives'))
 
-def scan_directory(directory_path, log_path):
-    with open(log_path, 'w', newline='') as log_file:
+
+def process_directory(directory_path, log_file_path):
+    with open(log_file_path, 'a', newline='') as log_file:
         log_writer = csv.writer(log_file)
-        log_writer.writerow(['Timestamp', 'Archive Path', 'Archive Name', 'File Path', 'File Name', 'Checksum'])
-        
-        for root in os.listdir(directory_path):
-            full_path = os.path.join(directory_path, root)
-            if os.path.isdir(full_path):
-                for root2, dirs, files in os.walk(full_path):
-                    for file_name in files:
-                        file_path = os.path.join(root2, file_name)
-                        if zipfile.is_zipfile(file_path) or rarfile.is_rarfile(file_path) or file_name.endswith('.tar') or file_name.endswith('.7z') or file_name.endswith('.msi'):
-                            process_archive(file_path, log_writer)
+        for root, dirs, files in os.walk(directory_path):
+            for file_name in files:
+                file_path = os.path.join(root, file_name)
+                process_archive(file_path, log_writer)
 
-# Test the program
-directory_path = '/srv/mergerfs/data/Software/sort2/ZIP/'
-log_path = '/srv/mergerfs/data/Software/sort2/ZIP/logfile.txt'
 
-scan_directory(directory_path, log_path)
+if __name__ == '__main__':
+    directory_path = input("Enter directory path: ")
+    log_file_path = input("Enter log file path: ")
+    if not os.path.isdir(directory_path):
+        print(f"{directory_path} is not a valid directory.")
+    else:
+        process_directory(directory_path, log_file_path)
