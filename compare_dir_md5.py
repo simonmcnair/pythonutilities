0c3378177b788f8505283ba0f9642e0859e9059b
101189766+simonmcnair@users.noreply.github.com <101189766+simonmcnair@users.noreply.github.com>
2023-07-07 17:29:26 +0100

    Updating file - Revision 5

--- compare_dir_md5.py
+++ compare_dir_md5.py
@@ -1,43 +1,69 @@
 import os
+import json
 import hashlib
 
-def get_sha256(file_path):
-    """Return the SHA256 hash of a file."""
-    sha256 = hashlib.sha256()
-    with open(file_path, 'rb') as f:
-        while True:
-            data = f.read(65536)
-            if not data:
-                break
-            sha256.update(data)
-    return sha256.hexdigest()
+# define the two directories to compare
+dir1 = "/srv/External_6TB_1/root/Videos/"
+dir2 = "/srv/mergerfs/data/Video2"
 
-def compare_directories(dir1, dir2):
-    """Compare the SHA256 hashes of files in two directories."""
-    dir1_files = set()
-    dir2_files = set()
-    for root, dirs, files in os.walk(dir1):
-        for name in files:
-            dir1_files.add(os.path.join(root, name))
-    for root, dirs, files in os.walk(dir2):
-        for name in files:
-            dir2_files.add(os.path.join(root, name))
-    dir1_hashes = {}
-    dir2_hashes = {}
-    for file_path in dir1_files:
-        dir1_hashes[file_path] = get_sha256(file_path)
-    for file_path in dir2_files:
-        dir2_hashes[file_path] = get_sha256(file_path)
-    for file_path, hash1 in dir1_hashes.items():
-        if file_path in dir2_hashes:
-            hash2 = dir2_hashes[file_path]
-            if hash1 != hash2:
-                print(f"File {file_path} differs.")
-        else:
-            print(f"File {file_path} not found in {dir2}.")
-    for file_path in dir2_hashes:
-        if file_path not in dir1_hashes:
-            print(f"File {file_path} not found in {dir1}.")
+# create a set of all filenames in both directories
+dir1_files = set(os.listdir(dir1))
+dir2_files = set(os.listdir(dir2))
+all_files = dir1_files.union(dir2_files)
 
-# Example usage:
-compare_directories("/path/to/dir1", "/path/to/dir2")
+# create an array to store the directory and filename for both directory trees
+file_array = []
+
+# add filenames to the file array with their respective directory
+for filename in all_files:
+    if filename in dir1_files:
+        print("Adding " + dir1 + filename)
+        file_array.append((dir1, filename))
+    if filename in dir2_files:
+        print("Adding " + dir2 + filename)
+        file_array.append((dir2, filename))
+
+# sort the file array by filename
+file_array.sort(key=lambda x: x[1])
+
+# read the existing hash json file or create a new one if it doesn't exist
+hash_file = "hashes.json"
+if os.path.isfile(hash_file):
+    with open(hash_file) as f:
+        hash_dict = json.load(f)
+else:
+    hash_dict = {}
+
+# loop through the file array and generate a hash for each pair of identical filenames
+for i in range(len(file_array)-1):
+    if file_array[i][1] == file_array[i+1][1]:
+        file1 = os.path.join(file_array[i][0], file_array[i][1])
+        file2 = os.path.join(file_array[i+1][0], file_array[i+1][1])
+        if file1 not in hash_dict and os.path.isfile(file1):
+            with open(file1, "rb") as f1:
+                print("Generating a hash for " + file1)
+                hash1 = hashlib.sha256(f1.read()).hexdigest()
+                print(hash1 + " is hash for " +file1)
+            hash_dict[file1] = hash1
+            with open(hash_file, "w") as f:
+                json.dump(hash_dict, f)
+        if file2 not in hash_dict and os.path.isfile(file2):
+            with open(file2, "rb") as f2:
+                print("Generating a hash for " + file2)
+                hash2 = hashlib.sha256(f2.read()).hexdigest()
+                print(hash2 + " is hash for " +file2)
+            hash_dict[file2] = hash2
+            with open(hash_file, "w") as f:
+                json.dump(hash_dict, f)
+
+# create a list of filenames where the hash is not the same
+different_hashes = []
+for i in range(len(file_array)-1):
+    if file_array[i][1] == file_array[i+1][1]:
+        file1 = os.path.join(file_array[i][0], file_array[i][1])
+        file2 = os.path.join(file_array[i+1][0], file_array[i+1][1])
+        if file1 in hash_dict and file2 in hash_dict and hash_dict[file1] != hash_dict[file2]:
+            different_hashes.append(file_array[i][1])
+
+# print the list of filenames with different hashes
+print(different_hashes)
