75120b559334c0e0ad2ec816611031bc04279574
101189766+simonmcnair@users.noreply.github.com <101189766+simonmcnair@users.noreply.github.com>
2023-07-07 17:29:26 +0100

    Updating file - Revision 6

--- compare_dir_md5.py
+++ compare_dir_md5.py
@@ -1,6 +1,7 @@
 import os
 import json
 import hashlib
+import datetime
 
 # define the two directories to compare
 dir1 = "/srv/External_6TB_1/root/Videos/"
@@ -17,10 +18,8 @@ file_array = []
 # add filenames to the file array with their respective directory
 for filename in all_files:
     if filename in dir1_files:
-        print("Adding " + dir1 + filename)
         file_array.append((dir1, filename))
     if filename in dir2_files:
-        print("Adding " + dir2 + filename)
         file_array.append((dir2, filename))
 
 # sort the file array by filename
@@ -34,6 +33,11 @@ if os.path.isfile(hash_file):
 else:
     hash_dict = {}
 
+# create a log file to store filenames
+log_file = "file_log.txt"
+with open(log_file, "w") as f:
+    f.write(f"Log created on {datetime.datetime.now()}\n\n")
+
 # loop through the file array and generate a hash for each pair of identical filenames
 for i in range(len(file_array)-1):
     if file_array[i][1] == file_array[i+1][1]:
@@ -41,9 +45,11 @@ for i in range(len(file_array)-1):
         file2 = os.path.join(file_array[i+1][0], file_array[i+1][1])
         if file1 not in hash_dict and os.path.isfile(file1):
             with open(file1, "rb") as f1:
+
                 print("Generating a hash for " + file1)
                 hash1 = hashlib.sha256(f1.read()).hexdigest()
                 print(hash1 + " is hash for " +file1)
+
             hash_dict[file1] = hash1
             with open(hash_file, "w") as f:
                 json.dump(hash_dict, f)
@@ -51,10 +57,14 @@ for i in range(len(file_array)-1):
             with open(file2, "rb") as f2:
                 print("Generating a hash for " + file2)
                 hash2 = hashlib.sha256(f2.read()).hexdigest()
-                print(hash2 + " is hash for " +file2)
+                print(hash1 + " is hash for " +file2)
             hash_dict[file2] = hash2
             with open(hash_file, "w") as f:
                 json.dump(hash_dict, f)
+        if hash_dict.get(file1) == hash_dict.get(file2):
+            print(f"Duplicate: {file1}, {file2}")
+            with open(log_file, "a") as f:
+                f.write(f"{file1}, {file2}\n")
 
 # create a list of filenames where the hash is not the same
 different_hashes = []
@@ -66,4 +76,6 @@ for i in range(len(file_array)-1):
             different_hashes.append(file_array[i][1])
 
 # print the list of filenames with different hashes
-print(different_hashes)
+print(f"Files with different hashes: {different_hashes}")
+with open(log_file, "a") as f:
+    f.write(f"\nFiles with different hashes: {different_hashes}")
